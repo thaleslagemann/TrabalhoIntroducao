a6dee67e7c577e5cf5d34d87c9e826f5d150d1e2
Thales Lagemann <37338846+thaleslagemann@users.noreply.github.com>
2019-04-01 23:02:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,29 @@
-//
-//  Trabalho 1 - Introdução à Ciência da Informação
 //  Felipe Machado, Matheus Filipe, Thales Lagemann
 //
 #include <stdio.h>
 
 int main()
 {
-	int dado[10], matrizG[7][4], i, j;
-	FILE *fp;
+	int dado[10], matrizG[7][4], i, j, matrizH[3][7];
+	FILE *fp, *fp2;
 
 	fp = fopen("matrizG", "r");
+	fp2 = fopen("matrizH", "r");
 
 	for (i = 0; i < 7; i++) {
 		for (j = 0; j < 4; j++) {
 			fscanf(fp, "%d", &matrizG[i][j]);
+			printf("%d ", matrizG[i][j]);
 		}
+		printf("\n");
+	}
+
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 7; j++) {
+			fscanf(fp2, "%d", &matrizH[i][j]);
+			printf("%d ", matrizH[i][j]);
+		}
+		printf("\n");
 	}
 	
 	printf (" Entre com 4 bits um de cada vez: \n");
